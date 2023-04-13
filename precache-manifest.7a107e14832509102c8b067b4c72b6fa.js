8362c3dcf79df4ab654a94f8edfdb2db4b877ba1
Akash-khandekar22 <aakash.khandekar21@gmail.com>
2023-04-14 00:52:43 +0530

    Updates

---
+++ precache-manifest.7a107e14832509102c8b067b4c72b6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ca770678a329045355f",
+    "url": "//React-Website/static/css/main.416ffafb.chunk.css"
+  },
+  {
+    "revision": "4ca770678a329045355f",
+    "url": "//React-Website/static/js/main.4ca77067.chunk.js"
+  },
+  {
+    "revision": "3a871b2980ab3371338b",
+    "url": "//React-Website/static/css/1.23848841.chunk.css"
+  },
+  {
+    "revision": "3a871b2980ab3371338b",
+    "url": "//React-Website/static/js/1.3a871b29.chunk.js"
+  },
+  {
+    "revision": "c918b2faa11e83b49c69",
+    "url": "//React-Website/static/js/runtime~main.c918b2fa.js"
+  },
+  {
+    "revision": "805eefdee2ac25c5e50456df0982bedf",
+    "url": "//React-Website/index.html"
+  }
+];
